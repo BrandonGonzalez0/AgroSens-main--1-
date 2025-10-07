a8c1c8893c919fe4c2f0e5a24a32bd6af6ffd8bc
BrandonGonzalez0 <brangm2004@gmail.com>
2025-10-07 19:57:10 -0300

    normal

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Este es el directorio donde se generarán los archivos
+    outDir: 'dist',  // Asegúrate de que esté configurado para generar en 'dist/'
+    rollupOptions: {
+      input: './index.html',  // Asegúrate de que esté apuntando al archivo correcto
+    },
   },
 });
