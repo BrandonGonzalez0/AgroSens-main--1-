284b05e509856f3c45aac9a329aaaff851019c01
BrandonGonzalez0 <brangm2004@gmail.com>
2025-10-07 20:56:07 -0300

    package original

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // 👈 importante para alternar con una clase
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
- 
+
